f77cb18edd4560cbd282315b54004be30f128677
Rafael Flores <fita500@gmail.com>
2021-09-17 18:50:40 -0600

    Add enter modifier

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -9,7 +9,8 @@ main
         input.input.is-large(
           type="text",
           placeholder="Buenas canciones",
-          v-model="searchQuery"
+          v-model="searchQuery",
+          @keyup.enter="search"
         )
         a.button.is-info.is-large(@click="search") Buscar
         a.button.is-danger.is-large &times;
